8725d55d68f76f05d28b8582b82dd42c6f5afaa9
Matt Ward <mattward@matts-air.lan>
2020-05-31 16:36:16 -0700

    create car.data.csv and initial R script to read the csv

---
+++ learning/books/practical_data_science_with_r/chapter2/ucicar.R
@@ -0,0 +1,8 @@
+#chapter 2 exercises
+
+uciCar <- read.table(
+  'datascience/learning/books/practical_data_science_with_r/chapter2/car.data.csv',
+  sep = ',',
+  header = TRUE,
+  stringsAsFactors = TRUE
+)
