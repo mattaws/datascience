e396f2501745e6adab53e0d3a8a631f768859006
Matt Ward <mattward@matts-air.lan>
2020-06-03 07:37:02 -0700

    minor - added new german data set and added dataset hierarchy

--- learning/books/practical_data_science_with_r/chapter2/ucicar.R
+++ learning/books/practical_data_science_with_r/chapter2/ucicar.R
@@ -1,12 +1,34 @@
-#chapter 2 exercises
+# Ref pages 31-33
+
+# load data
 
 uciCar <- read.table(
-  'datascience/learning/books/practical_data_science_with_r/chapter2/car.data.csv',
+  'datascience/learning/books/practical_data_science_with_r/chapter2/datasets/car.data.csv',
   sep = ',',
   header = TRUE,
   stringsAsFactors = TRUE
 )
 
+#Examine data with viewer
 View(uciCar)
 
-#Test Git
+#tells what kind of object you have
+class(uciCar)
+
+#tells how many rows and columns the df has
+dim(uciCar)
+
+#shows top few rows of data
+head(uciCar)
+
+#provides documentation for object
+help(class(uciCar))
+
+#gives the structure of an object
+str(uciCar)
+
+#provides summary of object
+summary(uciCar)
+
+#prints all the data
+print(uciCar)
